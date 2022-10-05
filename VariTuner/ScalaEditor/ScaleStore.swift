9ecbfa4cd6425c4888f02f19ff4e7d265302c275
aleksuuu <qwu2@berklee.edu>
2022-10-05 11:09:06 -0400

    Added 24-12 for testing

--- VariTuner/ScalaEditor/ScaleStore.swift
+++ VariTuner/ScalaEditor/ScaleStore.swift
@@ -80,7 +80,7 @@ class ScaleStore: ObservableObject {
     private func loadTestScales() {
         factoryScales.insert(
             Scale(name: "12-12_sharps",
-                  description: "12 out of 12-tET, the most boring tuning (preferring sharps)",
+                  description: "12 out of 12edo, the most boring tuning (preferring sharps)",
                   notes: [
                     Scale.Note(name: "C", cents: 0),
                     Scale.Note(name: "C \u{E262}", cents: 100),
@@ -94,6 +94,37 @@ class ScaleStore: ObservableObject {
                     Scale.Note(name: "A", cents: 900),
                     Scale.Note(name: "A \u{E262}", cents: 1000),
                     Scale.Note(name: "B", cents: 1100),
+                    Scale.Note(name: "C", cents: 1200)
+                  ]), at: 0
+        )
+        factoryScales.insert(
+            Scale(name: "24-12_sharps",
+                  description: "24 out of 12edo (preferring sharps)",
+                  notes: [
+                    Scale.Note(name: "C", cents: 0),
+                    Scale.Note(name: "C \u{E282}", cents: 50),
+                    Scale.Note(name: "C \u{E262}", cents: 100),
+                    Scale.Note(name: "C \u{E283}", cents: 150),
+                    Scale.Note(name: "D", cents: 200),
+                    Scale.Note(name: "D \u{E282}", cents: 250),
+                    Scale.Note(name: "D \u{E262}", cents: 300),
+                    Scale.Note(name: "D \u{E283}", cents: 350),
+                    Scale.Note(name: "E", cents: 400),
+                    Scale.Note(name: "E \u{E282}", cents: 450),
+                    Scale.Note(name: "F", cents: 500),
+                    Scale.Note(name: "F \u{E282}", cents: 550),
+                    Scale.Note(name: "F \u{E262}", cents: 600),
+                    Scale.Note(name: "F \u{E283}", cents: 650),
+                    Scale.Note(name: "G", cents: 700),
+                    Scale.Note(name: "G \u{E282}", cents: 750),
+                    Scale.Note(name: "G \u{E262}", cents: 800),
+                    Scale.Note(name: "G \u{E283}", cents: 850),
+                    Scale.Note(name: "A", cents: 900),
+                    Scale.Note(name: "A \u{E282}", cents: 950),
+                    Scale.Note(name: "A \u{E262}", cents: 1000),
+                    Scale.Note(name: "A \u{E283}", cents: 1050),
+                    Scale.Note(name: "B", cents: 1100),
+                    Scale.Note(name: "B \u{E282}", cents: 1150),
                     Scale.Note(name: "C", cents: 1200)
                   ]), at: 0
         )
